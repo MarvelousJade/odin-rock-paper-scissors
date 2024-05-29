655f81bd831b980ea6899291e6078b8d3a100dc5
shaoyu <162636625+MarvelousJade@users.noreply.github.com>
2024-05-29 17:11:42 -0400

    Creat function to get human choice

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 console.log("Hello, World");
 
 function getComputerChoice() {
-    let randomInt = Math.floor(Math.random() * 3);
-    let computerChoice = "Rock";
-    if (randomInt === 1) {
-        computerChoice = "Paper";
-    } else if (randomInt === 2) {
-        computerChoice = "Scissors";
-    } 
+  let randomInt = Math.floor(Math.random() * 3);
+  let computerChoice = "Rock";
+  if (randomInt === 1) {
+    computerChoice = "Paper";
+  } else if (randomInt === 2) {
+    computerChoice = "Scissors";
+  } 
+  return computerChoice;
 }
 
-getComputerChoice();
+function getHumanChoice() {
+  let rightChoice = false;
+
+  while (!rightChoice) {
+    humanChoice = prompt("What's your choice? (Choose between Rock, Paper and Scissors.)");
+    if (humanChoice === "Rock" || humanChoice === "Paper" || humanChoice === "Scissors") {
+      rightChoice = true;
+    } 
+  }
+
+  return humanChoice;
+}
+
+getHumanChoice();
